6fad366e8051ef1bfcf3eaa1d4fe0d99b6abe768
EmilioATC <142368988+EmilioATC@users.noreply.github.com>
2025-01-03 22:36:19 -0600

    Se implementaron rutas con nombre

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/home/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-
   static const String name = 'home_screen';
 
   const HomeScreen({super.key});
@@ -20,9 +20,8 @@ class HomeScreen extends StatelessWidget {
 }
 
 class _HomeView extends StatelessWidget {
-
   const _HomeView();
-  
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -45,16 +44,23 @@ class _CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      leading: Icon(menuItem.icon, color: colors.primary,),
-      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      leading: Icon(
+        menuItem.icon,
+        color: colors.primary,
+      ),
+      trailing: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: colors.primary,
+      ),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-         Navigator.pushNamed(context, menuItem.link);
+        // context.pushNamed(CardsScreen.name);
+        context.push( menuItem.link);
+        //  Navigator.pushNamed(context, menuItem.link);
         // Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ButtonsScreen()));
       },
     );
