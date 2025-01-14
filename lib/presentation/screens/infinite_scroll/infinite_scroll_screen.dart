f2437384bb56953546fdfcea50af6ae15447bad4
EmilioATC <142368988+EmilioATC@users.noreply.github.com>
2025-01-13 22:41:57 -0600

    Se realizó el infinite scroll

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,13 +1,151 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
-
+class InfiniteScrollScreen extends StatefulWidget {
   static const name = 'infinite_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+
+  ColorScheme get colors => Theme.of(context).colorScheme;
+
+  void moveScrollToBottom() {
+    if( scrollController.position.pixels + 100 <= scrollController.position.maxScrollExtent ) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 120, 
+      duration: const Duration(milliseconds: 300), 
+      curve: Curves.fastOutSlowIn
+    );
+
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map(
+      (e) => lastId + e,
+    ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >=
+          scrollController.position.maxScrollExtent - 500) {
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2));
+    addFiveImages();
+    isLoading = false;
+
+    if (!mounted) return;
+    setState(() {});
+    moveScrollToBottom();
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() { });
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+
+    final lastId = imagesIds.last;
+    isLoading = false;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+
+    setState(() {
+    });
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // appBar: AppBar(
+      //   title: const Text('Infinite Scroll'),
+      // ),
+      //https://picsum.photos/500/300?random=$index'
+      body: Stack(children: [
+        MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          removeBottom: true,
+          child: RefreshIndicator(
+            onRefresh: onRefresh,
+            edgeOffset: 10,
+            strokeWidth: 2,
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: imagesIds.length,
+              itemBuilder: (context, index) {
+                return FadeInImage(
+                  height: 250,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  placeholder:
+                      const AssetImage('assets/images/jar-loading.gif'),
+                  image: NetworkImage(
+                      'https://picsum.photos/id/${imagesIds[index]}/500/300'),
+                );
+              },
+            ),
+          ),
+        ),
+        if (isLoading)
+          Positioned(
+            bottom: 30.0,
+            left: 0.0,
+            right: 0.0,
+            child: Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                height: 30,
+                width: 30,
+                child: FadeInUp(
+                    child: CircularProgressIndicator(
+                        color: colors.primary, strokeWidth: 3)),
+              ),
+            ),
+          ),
+      ]),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print(isLoading);
+          context.pop();
+        },
+        // child: const Icon(Icons.arrow_back_ios_new_outlined),
+        child: isLoading
+            ? SpinPerfect(
+                infinite: true, child: const Icon(Icons.refresh_rounded))
+            : FadeIn(child: const Icon(Icons.arrow_back_ios_new_outlined)),
+      ),
+    );
   }
 }
